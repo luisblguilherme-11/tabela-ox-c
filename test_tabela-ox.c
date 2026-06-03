ad5cea5c241f8e62e02a65fa359de9052c1fa63c
Luís Guilherme <luis.bl.guilherme@gmail.com>
2026-06-03 18:56:19 -0300

    test_tabela-ox.c

---
+++ test_tabela-ox.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#include "function_validar.c"
+
+int main()
+{
+    printf("%d\n", validar('X'));
+    printf("%d\n", validar('O'));
+    printf("%d\n", validar('B'));
+
+    printf("Criado por Luís Guilherme\n");
+    return 0;
+}
