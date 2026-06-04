d230084377cb0c76f9861c89677ec5c11abf6d2b
Luís Guilherme <luis.bl.guilherme@gmail.com>
2026-06-04 18:03:50 -0300

    test_tabela-ox.c

--- test_tabela-ox.c
+++ test_tabela-ox.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
+#include <assert.h>
 
 #include "function_validar.c"
 
 int main()
 {
-    printf("%d\n", validar('X'));
-    printf("%d\n", validar('O'));
-    printf("%d\n", validar('B'));
+    assert(validar('X') == 1);
+    assert(validar('O') == 1);
+
+    assert(validar('B') == 0);
+    assert(validar('x') == 0);
+    assert(validar('o') == 0);
+    assert(validar('1') == 0);
+    assert(validar(' ') == 0);
 
     printf("Criado por Luís Guilherme\n");
     return 0;
